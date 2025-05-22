eeafd6f3d7dc6b7f93d705c4e285647065b57874
leticiacamillo686 <leticiacamillo@meumarketingcontabil.com>
2025-05-22 13:59:56 -0300

    Add files via upload

--- sugerir-dominios/app.py
+++ sugerir-dominios/app.py
@@ -32,4 +32,6 @@ def verificar_dominios():
     return jsonify({"dominios_disponiveis": disponiveis[:3]})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
